08348ab8d4d1dd35984cff76a3f324e503afcd65
honzi <czechinthemail@gmail.com>
2017-10-15 18:05:25 -0700

    moved uncommon functions to data.js

---
+++ js/data.js
@@ -0,0 +1,42 @@
+'use strict';
+
+function create_squares(){
+    var colorsquares = [];
+    var loop_counter = core_storage_data['square-count'];
+    do{
+        colorsquares.push('<input class=gridbutton id=' + loop_counter + ' type=button>');
+    }while(loop_counter--);
+
+    document.getElementById('colorsquares').innerHTML = colorsquares.join('');
+}
+
+function random_color(){
+    changed_color = '#' + core_random_hex();
+    default_color = '#' + core_random_hex();
+}
+
+function update(){
+    if(!core_menu_open){
+        update_counter += 1;
+        if(update_counter > core_storage_data['square-count'] / 2){
+            update_counter = 1;
+        }
+
+        var loop_counter = core_storage_data['square-count'];
+        do{
+            document.getElementById(core_storage_data['square-count'] - loop_counter).style.background =
+              loop_counter % update_counter === 0
+                ? default_color
+                : changed_color;
+        }while(loop_counter--);
+    }
+
+    window.setTimeout(
+      update,
+      core_storage_data['frame-ms']
+    );
+}
+
+var changed_color = '#000';
+var default_color = '#000';
+var update_counter = 0;
--- js/main.js
+++ js/main.js
@@ -1,20 +1,5 @@
 'use strict';
 
-function create_squares(){
-    var colorsquares = [];
-    var loop_counter = core_storage_data['square-count'];
-    do{
-        colorsquares.push('<input class=gridbutton id=' + loop_counter + ' type=button>');
-    }while(loop_counter--);
-
-    document.getElementById('colorsquares').innerHTML = colorsquares.join('');
-}
-
-function random_color(){
-    changed_color = '#' + core_random_hex();
-    default_color = '#' + core_random_hex();
-}
-
 function repo_init(){
     core_repo_init({
       'info': '<input id=reset type=button value=Reset><input id=random-color type=button value="Random Color">',
@@ -39,29 +24,3 @@ function repo_init(){
         core_escape();
     };
 }
-
-function update(){
-    if(!core_menu_open){
-        update_counter += 1;
-        if(update_counter > core_storage_data['square-count'] / 2){
-            update_counter = 1;
-        }
-
-        var loop_counter = core_storage_data['square-count'];
-        do{
-            document.getElementById(core_storage_data['square-count'] - loop_counter).style.background =
-              loop_counter % update_counter === 0
-                ? default_color
-                : changed_color;
-        }while(loop_counter--);
-    }
-
-    window.setTimeout(
-      update,
-      core_storage_data['frame-ms']
-    );
-}
-
-var changed_color = '#000';
-var default_color = '#000';
-var update_counter = 0;
