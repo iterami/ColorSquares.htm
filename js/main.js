802897df324d71d722c63e594165ec9549009fc3
honzi <czechinthemail@gmail.com>
2017-10-24 21:15:41 -0700

    changed info-events to events

--- js/main.js
+++ js/main.js
@@ -2,26 +2,26 @@
 
 function repo_init(){
     core_repo_init({
+      'events': {
+        'random-color': {
+          'onclick': function(){
+              random_color();
+              core_escape();
+          },
+        },
+        'reset': {
+          'onclick': function(){
+              update_counter = 0;
+              core_escape();
+          },
+        },
+      },
       'globals': {
         'changed_color': '#000',
         'default_color': '#000',
         'update_counter': 0,
       },
       'info': '<input id=reset type=button value=Reset><input id=random-color type=button value="Random Color">',
-      'info-events': {
-        'random-color': {
-          'todo': function(){
-              random_color();
-              core_escape();
-          },
-        },
-        'reset': {
-          'todo': function(){
-              update_counter = 0;
-              core_escape();
-          },
-        },
-      },
       'storage': {
         'square-count': 624,
       },
